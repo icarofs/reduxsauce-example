0c706c38178f89cc200b9ea3bce64633c0124a19
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-08-06 22:46:40 -0300

    WIP - configuração inicial do redux, redux-saga, reactotron(saga e redux)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import history from './services/history';
-
+import './config/ReactotronConfig';
 import Routes from './routes';
+import store from './store';
 
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-    </Router>
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+  /**
+   * Criar um reducer para remover warning do console
+   */
+});
